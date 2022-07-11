5432c08a403a14ccf3551ab08bc882278a466c86
Justinas <68568214+JustinasPuzas@users.noreply.github.com>
2022-07-11 22:54:55 +0300

    Added wild card to Routes
    Added wildcard to Routes to prevent entering invalid paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import NavBar from "./Components/NavBar/NavBar";
 import HomePage from "./Pages/HomePage/HomePage";
@@ -8,15 +8,15 @@ import AboutPage from "./Pages/AboutPage/AboutPage";
 import WorksPage from "./Pages/WorksPage/WorksPage";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <NavBar />
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/works" element={<WorksPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         {/* <Footer /> */}
       </BrowserRouter>
